9e98cac50cd702cfebf72f0a5b20b8bd81bcf70c
Jesse Huang <jesshuang5733@gmail.com>
2025-10-04 18:31:59 -0400

    feat: added linting workflows

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import js from '@eslint/js';
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        console: 'readonly',
+        process: 'readonly'
+      }
+    },
+    rules: {
+      'no-console': 'off', // Allow console statements for Discord bot logging
+      'no-unused-vars': 'error',
+      'no-undef': 'error',
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single'],
+      'indent': ['error', 4]
+    }
+  }
+];
